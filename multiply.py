9b1281189c1a76d571a12bf72a5fe49b3c53925a
kenechris2003 <kenechris2000@gmail.com>
2023-10-31 19:16:12 +0100

    update

---
+++ multiply.py
@@ -0,0 +1,6 @@
+for i in range(1,13):
+    print("************************************************")
+    for j in range(1,13):
+        product = i * j
+        print(i , "x" , j , "=" , product)
+        
